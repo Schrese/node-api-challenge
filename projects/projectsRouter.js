9a89ccc84112d316b800d81649d409012299d2e6
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-17 12:01:44 -0500

    get by id works in projects router

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,11 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-//GET all projects (get())
+const Projects = require('../data/helpers/projectModel.js')
 
+//GET all projects (get())
+//does not work, not sure why. I don't think there is a "get all" in this db????
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(proj => {
+            res.status(201).json(proj)
+        })
+        .catch(err => {
+            console.log('error getting all projects', err)
+            res.status(500).json({ errorMessage: "The projects could not be found." })
+        })
+})
 
 //GET projects by id (get()) - takes in an id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
 
+    Projects.get(id)
+        .then(proj => {
+            res.status(200).json(proj)
+        })
+        .catch(err => {
+            console.log('error getting project from this id', err)
+            res.status(500).json({ errorMessage: "The project associated with that id could not be found." })
+        })
+})
 
 //GET project actions (getProjectActions()) - takes in PROJECT ID
 
