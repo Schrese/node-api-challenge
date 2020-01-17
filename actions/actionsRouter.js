3330982fbd1899a0a78b526f7cb35b19cbf79ac9
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-17 13:11:16 -0500

    editing actions works

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     const newAct = req.body;
     if (newAct.description.length === 0 || newAct.notes.length === 0) {
-        res.status(400).json({ message: 'Please provied notes and description for this Action' })
+        res.status(400).json({ message: 'Please provie notes and description for this Action' })
     } else {
         Projects.get(newAct.project_id)
             Actions.insert(newAct)
@@ -52,6 +52,24 @@ router.post('/', (req, res) => {
 
 
 //PUT updates an action (update()) - gets 2 args (id, changes)
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedAct = req.body;
+    if (updatedAct.description.length === 0 || updatedAct.notes.length === 0) {
+        res.status(400).json({ message: 'Please provide notes and description for this action' })
+    } else {
+        Actions.update(id, updatedAct)
+            .then(newA => {
+                console.log('New Action', newA)
+                res.status(200).json({ message: 'Action successfully updated!' })
+            })
+            .catch(err => {
+                console.log('error updating action', err)
+                res.status(500).json({ errorMessage: 'The action could not be modified' })
+            })
+    }
+   
+})
 
 //DELETE removes an action by id (remove()) - takes in an id
 
