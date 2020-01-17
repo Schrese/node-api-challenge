a8cbc3feec5fc0f3e8f724bfd58e437e02f50de2
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-17 12:44:05 -0500

    getting all actions and getting action by id works

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -2,11 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
-//GET all actions by project id (get())
+const Actions = require('../data/helpers/actionModel.js');
 
+
+//GET all actions by project id (get())
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(acts => {
+            res.status(200).json(acts)
+        })
+        .catch(err => {
+            console.log('error getting all actions', err)
+            res.status(500).json({ errorMessage: 'Could not retrieve Actions' })
+        })
+})
 
 //GET actions by id (get()) - takes in an id
-
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Actions.get(id)
+        .then(act => {
+            res.status(200).json(act)
+        })
+        .catch(err => {
+            console.log('error getting action by id', err)
+            res.status(500).json({ errorMessage: 'Could not retrieve this Action' })
+        })
+})
 
 //POST adds an action (insert()) - description and notes required, completed is optional
 
